fe0b8b4479c2d3d9d332825df9e53b7c2900effc
Maksim Binger <mbinger@deloitte.de>
2018-01-02 13:54:25 +0100

    Update to OWIN for Service

---
+++ Service/App_Start/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using log4net;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Service.Startup))]
+namespace Service
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            LogConfig.RegisterLogging();
+            DependenciesConfig.RegisterDependencies();
+            MappingConfig.RegisterMapping();
+
+            var log = LogManager.GetLogger(typeof(WebApiApplication));
+            log.Info("Applicaton startet");
+        }
+    }
+}
--- Service/Global.asax.cs
+++ Service/Global.asax.cs
@@ -1,25 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
-using System.Diagnostics;
-using log4net;
-
-namespace Service
+﻿namespace Service
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
-        protected void Application_Start()
-        {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            LogConfig.RegisterLogging();
-            DependenciesConfig.RegisterDependencies();
-            MappingConfig.RegisterMapping();
-            
-            var log = LogManager.GetLogger(typeof(WebApiApplication));
-            log.Info("Applicaton startet");
-        }
     }
 }
