59bdb5f74f865d486781331b055419ceb7ef15fd
Maksim Binger <mbinger@deloitte.de>
2018-01-02 11:48:16 +0100

    Update WebApp to OWIN

---
+++ WebApp/App_Start/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApp.Startup))]
+namespace WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
--- WebApp/Global.asax.cs
+++ WebApp/Global.asax.cs
@@ -12,10 +12,6 @@ namespace WebApp
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
