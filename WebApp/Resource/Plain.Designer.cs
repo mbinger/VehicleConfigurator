8168c6dfc4cf4f1b2a98fea8f2876b912b06caea
Maksim Binger <mbinger@deloitte.de>
2018-12-06 12:15:14 +0100

    Update copyright texts

--- WebApp/Resource/Plain.Designer.cs
+++ WebApp/Resource/Plain.Designer.cs
@@ -19,7 +19,7 @@ namespace WebApp.Resource {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Plain {
@@ -313,7 +313,7 @@ namespace WebApp.Resource {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to * This program is not a commercial application and is used only for educational purposes. The entire range and prices are fictitious. You can not purchase a real car with this program. 2017 M.Binger.
+        ///   Looks up a localized string similar to * This program is not a commercial application and is used only for educational purposes. The entire range and prices are fictitious. You can not purchase a real car with this program. 2018 M.Binger.
         /// </summary>
         public static string Trademark {
             get {
