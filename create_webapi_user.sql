d247d7188723162487ab674f65a82a637389fdc2
Maksim Binger <mbinger@deloitte.de>
2018-12-06 11:57:37 +0100

    Modify SQL Skript to create SQL service user

--- create_webapi_user.sql
+++ create_webapi_user.sql
@@ -1,23 +1,24 @@
 /*use master*/
 CREATE LOGIN webapiuser WITH PASSWORD = /*your password here*/
 
-/*use VC database*/
-CREATE ROLE webapi
-GRANT SELECT ON dbo.AdditionalEquipmentItems TO webapi
-GRANT SELECT ON dbo.Cars TO webapi
-GRANT SELECT ON dbo.Colors TO webapi
-GRANT SELECT ON dbo.ColorTypeRefs TO webapi
-GRANT SELECT ON dbo.Engines TO webapi
-GRANT SELECT ON dbo.FuelTypeRefs TO webapi
-
-GRANT INSERT, SELECT, UPDATE, DELETE ON dbo.OrderAdditionalEquipmentItems TO webapi
-GRANT INSERT, SELECT, UPDATE, DELETE ON dbo.Orders TO webapi
-
-GRANT SELECT ON dbo.OrderStatusRefs TO webapi
-GRANT SELECT ON dbo.Rims TO webapi
-GRANT SELECT ON dbo.RimTypeRefs TO webapi
-
-GRANT SELECT ON dbo.__MigrationHistory TO webapi
+/*create the user as db_owner to be able to set-up the database schema*/
 
 CREATE USER webapiuser FOR LOGIN webapiuser WITH DEFAULT_SCHEMA = dbo
+EXEC sp_addrolemember N'db_owner', N'webapiuser'
+GRANT CONNECT TO webapiuser
+
+/*todo: now run the service and let hit create the database*/
+
+EXEC sp_droprolemember N'db_owner', N'webapiuser'
+
+/*use VC database*/
+CREATE ROLE webapi
+
+/*allow select all*/
+GRANT SELECT ON SCHEMA::dbo TO webapi
+
+/*allow modify order and its equipment items*/
+GRANT INSERT, UPDATE, DELETE ON dbo.OrderAdditionalEquipmentItems TO webapi
+GRANT INSERT, UPDATE, DELETE ON dbo.Orders TO webapi
+
 EXEC sp_addrolemember N'webapi', N'webapiuser'
